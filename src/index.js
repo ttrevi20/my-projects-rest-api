c231183d644180c027990c2265025cd9b3a6339c
ttrevi20 <ttrevi20@stu.southtexascollege.edu>
2026-03-15 17:09:43 -0500

    initial API layout

---
+++ src/index.js
@@ -0,0 +1,33 @@
+import { Hono } from 'hono'
+
+const app = new Hono()
+
+let projects = [
+  {id: 1, 
+    name: "Project 1",
+    description: "First Project on Hono API",
+  },
+];
+
+let nextProjectId = 2
+
+app.get("/", (c) => {
+  return c.json({msg: "Hello There!"});
+});
+
+app.get("/projects", (c) => {
+  return c.json(projects);
+});
+
+app.post("/projects", async (c) => {
+  const payload = c.req.json()
+  projects.push({
+    id: nextProjectId,
+    ...payload
+})
+nextProjectId++
+
+return c.json({ ...payload, id: nextProjectId - 1 });
+});
+
+export default app;
